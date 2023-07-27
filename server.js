9bf5b1275a5e96a2ad438f2f85278816ea66d9f5
Wen <wenliangsu65018@gmail.com>
2023-07-27 10:10:34 +0800

    Feat: install the npm and socket.io

---
+++ server.js
@@ -0,0 +1,11 @@
+const server = require('http').createServer();
+const io = require('socket.io')(server);
+
+const PORT = 3000;
+
+server.listen(PORT);
+console.log(`Server listening on port ${PORT}`);
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+});
