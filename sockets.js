f526d27d6f53afa9ee52ee15862cce3229499e2b
Wen <wenliangsu65018@gmail.com>
2023-07-30 01:07:47 +0800

    Feat: set the room number for the server-side

--- sockets.js
+++ sockets.js
@@ -1,40 +1,47 @@
 let readyPlayerCount = 0;
 
 function listen(io) {
-  // note 命名後可以擁有相同的instance可以使用
+  // note 對channel進行Namespace後可以擁有相同的instance可使用
   const pongNamespace = io.of('/pong');
+
   pongNamespace.on('connection', (socket) => {
+    let room;
+
     // note 所有players連線會顯示
     console.log('a user connected', socket.id);
 
     // note 當有player連線時，將readyPlayerCount+1
     socket.on('ready', () => {
-      console.log('Player ready', socket.id);
+      // set room number
+      room = 'room' + Math.floor(readyPlayerCount / 2);
+      socket.join(room);
+
+      console.log('Player ready', socket.id, room);
 
       readyPlayerCount++;
 
       // note 湊到兩個的話就開始發送startGame的訊號給script.js
       if (readyPlayerCount % 2 === 0) {
         // broadcast('startGame')
-        pongNamespace.emit('startGame', socket.id);
+        pongNamespace.in(room).emit('startGame', socket.id);
       }
     });
 
     // Paddle synchronization 
     socket.on('paddleMove', (paddleData) => {
-      // note socket.broadcast.emit()是送出訊息給全部的client，除了發送者之外
-      socket.broadcast.emit('paddleMove', paddleData);
+      // note 使用to(room)來將訊息給此房間的client，除了發送者之外
+      socket.to(room).emit('paddleMove', paddleData);
     });
 
     // Ball synchronization
     socket.on('ballMove', (ballData) => {
-      socket.broadcast.emit('ballMove', ballData);
+      socket.to(room).emit('ballMove', ballData);
     });
 
     socket.on('disconnect', (reason) => {
       console.log(`Client ${socket.id} disconnected: ${reason}`);
+      socket.leave(room);
     });
-
   });
 }
 
